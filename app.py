5a69380861a9574561cd86a93291960f6f0d761a
sonu prakash k.s <ssonuprakashks@gmail.com>
2026-02-09 10:29:15 +0530

    commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+from azure.ai.inference import ChatCompletionsClient
+from azure.ai.inference.models import SystemMessage, UserMessage
+from azure.core.credentials import AzureKeyCredential
+
+endpoint = "https://models.github.ai/inference"
+model = "gpt-4o-mini"
+from dotenv import load_dotenv
+load_dotenv()
+token = os.environ["GITHUB_TOKEN"]
+
+client = ChatCompletionsClient(
+    endpoint=endpoint,
+    credential=AzureKeyCredential(token),
+)
+
+response = client.complete(
+    messages=[
+        SystemMessage("You are a helpful assistant."),
+        UserMessage("What is the capital of France?"),
+    ],
+    model=model
+)
+
+print(response.choices[0].message.content)
+
